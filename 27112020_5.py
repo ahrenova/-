9e6fc0af49e40168d69937e06e9a6c1c86f32f65
ahrenova <74719348+ahrenova@users.noreply.github.com>
2020-12-01 18:39:21 +0300

    Add files via upload

--- 27112020_5.py
+++ 27112020_5.py
@@ -1,15 +1,18 @@
-sum_number = 0
-for i in range(100):
-    numbers=input('Enter Number use space')
-    # if numbers == 'a':
-    #     break
-    # print(numbers)
-    # print(numbers.split())
-    for number in numbers.split():
+def get_sum(start_sum):
+    for i in range(100):
+        numbers = input('Enter Number use space')
+        # if numbers == 'a':
+        #     break
+        # print(numbers)
+        # print(numbers.split())
+        for number in numbers.split():
+            if number == 'a':
+                break
+            sum_number += float(number)
         if number == 'a':
             break
-        sum_number += float(number)
-    if number == 'a':
-        break
-    print(sum_number)
+        print(sum_number)
+
+sum_number = 0
+get_sum(sum_number)
 print(sum_number)
